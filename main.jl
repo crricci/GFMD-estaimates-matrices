66c53d7079d92a072f6dca3e28c3095368023f64
Cristiano Ricci <cristiano.ricci6@gmail.com>
2022-01-25 15:58:25 +0100

    finished export aggregate matrices into csv

--- main.jl
+++ main.jl
@@ -6,6 +6,7 @@ using BenchmarkTools
 using LinearAlgebra
 using ForwardDiff
 using ProgressMeter
+using CSV, DataFrames
 
 include("L_GFD.jl")
 include("L_AggregateMatrix.jl")
@@ -13,8 +14,16 @@ include("L_AggregateMatrix.jl")
 include("R_DEBUG.jl")
 
 function main()
+    muniCSV = CSV.read("coordinates_municipalities.csv",DataFrame)
+    locations = hcat(muniCSV.V1,muniCSV.V2)
+    Mx, My, Mxx, Myy, Mxy = compute_MDiff(locations)
 
-    return 0 
+    CSV.write("Mx.csv",Tables.table(vec(Mx)),header=false)
+    CSV.write("My.csv",Tables.table(vec(My)),header=false)
+    CSV.write("Mxx.csv",Tables.table(vec(Mxx)),header=false)
+    CSV.write("Myy.csv",Tables.table(vec(Myy)),header=false)
+    CSV.write("Mxy.csv",Tables.table(vec(Mxy)),header=false)
+    
 end
 
 function testLocations(N)
